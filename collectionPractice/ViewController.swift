89e8bd8b28229c9baf75e55baa0bd47bc2fa9603
Nikunj Prajapati <prajapatimurli2000@gmail.com>
2020-07-13 12:18:50 +0530

    Initial Commit

---
+++ collectionPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionPractice
+//
+//  Created by Nikunj Prajapati on 13/07/20.
+//  Copyright © 2020 Nikunj Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
